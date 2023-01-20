da45ddffa74400672e4f46d9d40b62c36aa330ca
quas-modo <211250106@smail.nju.edu.cn>
2023-01-20 23:15:23 +0800

    feat: single radio feature, just a git test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.scss'
+import Radio from "./components/Radio/radio"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1 className="App__title">Hello violetUI !</h1>
       <h2 className="App__subtitle">项目已经配置好，请往我身上添加组件吧！</h2>
+      <Radio value="test">Test</Radio>
     </div>
   )
 }
---
+++ src/components/Radio/radio.tsx
@@ -0,0 +1,65 @@
+import React,{ReactNode, useState, useRef} from "react";
+import classNames from "classnames";
+
+export interface RadioProps{
+    className?: string;
+    value? :string;
+    checked?: boolean;
+    disabled?: boolean;
+    children?: ReactNode;
+    size?: string;
+    onChange?: (event: React.FormEvent<HTMLInputElement>) => void;
+}
+
+const Radio: React.FC<RadioProps> = (props) =>{
+    const{
+        className,
+        value,
+        disabled,
+        children,
+        size,
+        onChange,
+        ...restProps
+    } = props
+
+    const [checked, setChecked] = useState(false);
+    const inputEl = useRef(null);
+
+    const classes = classNames('radio', className, {
+        ['radio-${size}']: size,
+        'disabled': disabled
+    })
+
+    const handleClick = (e: any) => {
+        if (disabled || checked) {
+            return;
+        }
+
+        if (!('checked' in props)) {
+            setChecked(true);
+        }
+
+    }
+
+
+    return(
+        <span onClick={(e) => {handleClick(e)}} >
+            <input type="radio"
+                   className={classes}
+                   disabled={disabled}
+                   value={value}
+            />
+            <span>{props.children}</span>
+        </span>
+
+
+    )
+
+}
+
+Radio.defaultProps = {
+    disabled: false,
+    checked: false
+}
+
+export default Radio;
