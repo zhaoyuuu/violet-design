52285695d6563bf1655c549c6920228bfa0a382e
quas-modo <211250106@smail.nju.edu.cn>
2023-01-28 22:43:07 +0800

    feat: radio and radioGroup storybook

---
+++ src/components/Radio/radioGroup.stories.tsx
@@ -0,0 +1,23 @@
+import Radio from './radio'
+import RadioGroup from './radioGroup'
+import React from 'react'
+
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+
+const radioGroupMeta: ComponentMeta<typeof Radio> = {
+    title: 'RadioGroup 导航菜单',
+    component: Radio
+}
+
+export default radioGroupMeta
+
+export const Default: ComponentStory<typeof RadioGroup> = (args) =>(
+    <>
+        <RadioGroup {...args}>
+            <Radio>1</Radio>
+            <Radio>2</Radio>
+            <Radio>3</Radio>
+        </RadioGroup>
+    </>
+)
+
