5487e02002fb8b9791ba56910d4bc5f562549838
yuhang <zyh200286@qq.com>
2023-01-29 20:10:00 +0800

    feat: cascader story

--- src/App.tsx
+++ src/App.tsx
@@ -17,60 +17,6 @@ interface Option {
 }
 
 function App() {
-  // InputNumber
-  //#region
-  const [value, setValue] = useState('1')
-  const onChange = (val: string) => {
-    setValue(val)
-  }
-  const onPressEnter = () => {
-    alert('press enter !')
-  }
-  //#endregion
-
-  // Cascader
-  //#region
-  const [cascaderValue, setCascaderValue] = useState<React.ReactNode[]>([])
-  const onCascaderChange = (value: React.ReactNode[]) => {
-    setCascaderValue(value)
-  }
-  const options: Option[] = [
-    {
-      value: 'zhejiang',
-      label: 'Zhejiang',
-      children: [
-        {
-          value: 'hangzhou',
-          label: 'Hangzhou',
-          children: [
-            {
-              value: 'xihu',
-              label: 'West Lake',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      value: 'jiangsu',
-      label: 'Jiangsu',
-      children: [
-        {
-          value: 'nanjing',
-          label: 'Nanjing',
-          children: [
-            {
-              value: 'zhonghuamen',
-              label: 'Zhong Hua Men',
-            },
-          ],
-        },
-      ],
-    },
-  ]
-
-  //#endregion
-
   return (
     <div className="App">
       <h1 className="App__title">Hello violetUI !</h1>
@@ -123,37 +69,6 @@ function App() {
       >
         Large Link
       </Button>
-      {/* <hr />
-      <Menu mode="vertical">
-        <MenuItem>first link</MenuItem>
-        <MenuItem>second link</MenuItem>
-        <MenuItem disabled>third link</MenuItem>
-        <SubMenu title="dropdown">
-          <MenuItem>dropdown 1</MenuItem>
-          <MenuItem>dropdown 2</MenuItem>
-          <MenuItem>dropdown 3</MenuItem>
-        </SubMenu>
-      </Menu> */}
-      {/* <hr />
-      <InputNumber
-        value={value}
-        onChange={onChange}
-        step={2}
-        max={10}
-        min={-10}
-        onPressEnter={onPressEnter}
-      /> */}
-      <hr />
-
-      <Cascader
-        value={cascaderValue}
-        onChange={onCascaderChange}
-        placeholder="请选择"
-        options={options}
-        // disabled={true}
-        // changeOnSelect={true}
-        // status="success"
-      />
     </div>
   )
 }
