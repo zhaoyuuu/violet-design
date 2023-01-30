9aa91bf574dfc6254ae6097205253b965c1c691e
violetwzj <809133018@qq.com>
2023-01-30 13:41:23 +0800

    fix: Modify the Button component

--- src/App.tsx
+++ src/App.tsx
@@ -90,34 +90,34 @@ function App() {
       </RadioGroup> */}
       <br />
       <Button className="custom">Hello</Button>
-      <Button btnType={ButtonType.Primary} disabled>
+      <Button btnType="primary" disabled>
         disabled button
       </Button>
       <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Primary}
+        size='lg'
+        btnType="primary"
         className="violetButton"
       >
         Large Primary
       </Button>
       <Button
-        size={ButtonSize.Small}
-        btnType={ButtonType.Danger}
+        size='sm'
+        btnType="danger"
         className="violetButton"
       >
         Small Danger
       </Button>
       <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Link}
+        size='lg'
+        btnType="link"
         href="http://www.baidu.com"
         className="violetButton"
       >
         Large Link
       </Button>
       <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Link}
+        size='lg'
+        btnType="link"
         disabled
         className="violetButton"
       >
