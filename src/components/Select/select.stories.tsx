ac1d516eacbdc1ae94d53c393124269e2e9db7e4
666laoyang <1071941886@qq.com>
2023-02-01 00:50:37 +0800

    feat: add select.stories.tsx

---
+++ src/components/Select/select.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { ComponentMeta } from '@storybook/react'
+
+import Select from './index'
+export default {
+  title: 'Select组件',
+  component: Select,
+  id: 'Select',
+  subcomponents: { Option: Select.Option },
+  // 让整个story包在width:350px的div内
+  decorators: [
+    Story => (
+      <div style={{ width: '350px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Select>
+
+export const DefaultSelect = (args: any) => (
+  <Select {...args} placeholder="请选择">
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+    <Select.Option value="disabled" disabled />
+    <Select.Option value="nihao5" />
+  </Select>
+)
+DefaultSelect.storyName = '默认的Select'
+
+export const MultipleSelect = (args: any) => (
+  <Select {...args} placeholder="多选框" multiple>
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+    <Select.Option value="nihao4" />
+    <Select.Option value="nihao5" />
+  </Select>
+)
+MultipleSelect.storyName = '支持多选的Select'
+
+export const DisabledSelect = (args: any) => (
+  <Select {...args} placeholder="禁选框" disabled>
+    <Select.Option value="nihao" />
+    <Select.Option value="nihao2" />
+    <Select.Option value="nihao3" />
+  </Select>
+)
+DisabledSelect.storyName = '被禁用的 Select'
