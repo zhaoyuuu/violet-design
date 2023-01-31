4f4ab213929eab9245d0cf92141f5dd7e52c7136
violetwzj <809133018@qq.com>
2023-01-31 17:14:30 +0800

    BREAKING CHANGE: Added From update value capability

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,22 +1,40 @@
-import React, {FC, ReactNode } from 'react';
-
-
+import React, { FC, ReactNode, createContext } from 'react'
+import useStore from './useStore'
 export interface FormProps {
-    name?: string;
-    children?: ReactNode;
+  name?: string
+  children?: ReactNode
 }
 
-export const Form: FC<FormProps> = (props) => {
-    const {name, children} = props
-    return (
-        <form name={name} className='violetForm'>
-            {children}
-        </form>
-    )
-} 
+export type IFormContext = Pick<
+  ReturnType<typeof useStore>,
+  'dispatch' | 'fields'
+>
+export const FormContext = createContext<IFormContext>({} as IFormContext)
+
+export const Form: FC<FormProps> = props => {
+  const { name, children } = props
+  const { form, fields, dispatch } = useStore()
+  const passedContext: IFormContext = {
+    dispatch,
+    fields,
+  }
+  return (
+    <>
+      <form name={name} className="violetForm">
+        <FormContext.Provider value={passedContext}>
+          {children}
+        </FormContext.Provider>
+      </form>
+      <div>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(fields)}</pre>
+        <pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(form)}</pre>
+      </div>
+    </>
+  )
+}
 
 Form.defaultProps = {
-    name: 'violetForm'
+  name: 'violet_form',
 }
 
 export default Form
--- src/components/Form/formItem.tsx
+++ src/components/Form/formItem.tsx
@@ -1,33 +1,81 @@
-import React, {FC, ReactNode } from 'react';
-import classNames from 'classnames';
-
+import React, { FC, ReactNode, useContext, useEffect } from 'react'
+import classNames from 'classnames'
+import { FormContext } from './form'
 export interface FormItemProps {
-    label?: string;
-    children?: ReactNode;
+  name: string
+  label?: string
+  children?: ReactNode
+  valuePropName?: string
+  trigger?: string
+  getValueFromEvent?: (event: any) => any
 }
 
-const FormItem: FC<FormItemProps> = (props) => {
-    const {label, children} = props
+const FormItem: FC<FormItemProps> = props => {
+  const { label, children, name, valuePropName, trigger, getValueFromEvent } =
+    props
+  const { dispatch, fields } = useContext(FormContext)
+  const rowClass = classNames('violetRow', {
+    '.violetRow--no__label': !label,
+  })
 
-    const rowClass = classNames('violet-row', {
-        'violet-row-no-label': !label,
+  useEffect(() => {
+    dispatch({ type: 'addField', name, value: { label, name, value: '' } })
+  }, [])
+  //获取store对应的value
+  const fieldState = fields[name]
+  const value = fieldState && fieldState.value
 
-    })
-    return (
-        <div className={rowClass}>
-            {
-                label && 
-                <div className='violet-form-item-label'>
-                    <label title={label}>
-                        {label}
-                    </label>
-                </div>
-            }
-            <div className='vilet-form-item'>
-                {children}
-            </div>
-        </div>
+  const onValueUpdate = (e: any) => {
+    const value = getValueFromEvent && getValueFromEvent(e)
+    console.log('new value', value)
+    dispatch({ type: 'updateValue', name, value })
+  }
+  // 1 手动的创建一个属性列表，需要有 value 以及 onChange 属性
+  const controlProps: Record<string, any> = {}
+  controlProps[valuePropName!] = value
+  controlProps[trigger!] = onValueUpdate
+  // 2 获取 children 数组的第一个元素
+  const childList = React.Children.toArray(children)
+  // 没有子组件
+  if (childList.length === 0) {
+    console.error(
+      'No child element found in Form.Item, please provide one form component'
     )
-} 
+  }
+  // 子组件大于一个
+  if (childList.length > 1) {
+    console.warn(
+      'Only support one child element in Form.Item, others will be omitted'
+    )
+  }
+  // 不是 ReactElement 的子组件
+  if (!React.isValidElement(childList[0])) {
+    console.error('Child component is not a valid React Element')
+  }
+
+  const child = childList[0] as React.ReactElement
+  // 3 cloneElement，混合这个child 以及 手动的属性列表
+  const returnChildNode = React.cloneElement(child, {
+    ...child.props,
+    ...controlProps,
+  })
+  return (
+    <div className={rowClass}>
+      {label && (
+        <div className="violetForm--item__label">
+          <label title={label}>{label}</label>
+        </div>
+      )}
+      <div className="violetForm--item ">{returnChildNode}</div>
+    </div>
+  )
+}
+
+FormItem.defaultProps = {
+  valuePropName: 'value',
+  trigger: 'onChange',
+  // validateTrigger: 'onBlur',
+  getValueFromEvent: e => e.target.value,
+}
 
 export default FormItem
