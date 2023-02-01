043a33eaee843b79e76ad9e89f2a8b44c4b215ef
yuhang <zyh200286@qq.com>
2023-02-01 20:15:22 +0800

    feat: optimize menu style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react'
 import Radio from './components/Radio/radio'
 import RadioGroup from './components/Radio/radioGroup'
 import InputNumber from './components/InputNumber/inputNumber'
-import Menu from './components/Menu/menu'
-import MenuItem from './components/Menu/menuItem'
-import SubMenu from './components/Menu/subMenu/subMenu'
-import Icon from './components/Icon'
+import Menu from './components/Menu'
 import Switch from './components/Switcher'
 import Tabs from './components/Tabs/tabs'
 import TabItem from './components/Tabs/tabItem'
@@ -17,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="App__title">Hello violetUI !</h1>
+
       <Switch disabled></Switch>
       <Input size="sm" />
       <Input size="lg" />
@@ -42,6 +40,16 @@ function App() {
       <Button size="lg" btnType="link" disabled className="violetButton">
         Large Link
       </Button>
+      <Menu mode="vertical">
+        <Menu.Item>active</Menu.Item>
+        <Menu.Item disabled>disabled</Menu.Item>
+        <Menu.Item>xyz</Menu.Item>
+        <Menu.SubMenu title="dropdown">
+          <Menu.Item>drop1</Menu.Item>
+          <Menu.Item>drop2</Menu.Item>
+          <Menu.Item>drop3</Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
     </div>
   )
 }
