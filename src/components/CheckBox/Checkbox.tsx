4422ecc0aa28baa90df051a1a68a3079154a5b25
zhubeijia <1904829708@qq.com>
2023-02-06 00:18:33 +0800

    fix: change to BEM format

--- src/components/CheckBox/Checkbox.tsx
+++ src/components/CheckBox/Checkbox.tsx
@@ -1,18 +1,18 @@
-
 import React, { useEffect, useState } from 'react'
-import './index.scss'
+import './_style.scss'
 
 const classnames = require('classnames')
 
-export interface Iprops {
-  type: string // 样式类型
-  checked: boolean // 是否选中
-  disabled: boolean // 是否禁用
-  value: string | number // 单独 value 值
-  name?: string // 原生 name 属性
-  label: string // label 名称
-  indeterminate?: any // 是否为统一全选控制按钮组件
+export interface CheckBoxProps {
+  type: string, // 样式类型
+  checked: boolean, // 是否选中
+  disabled: boolean, // 是否禁用
+  value: string | number, // 单独 value 值
+  name?: string, // 原生 name 属性
+  label: string, // label 名称
+  indeterminate?: any, // 是否为统一全选控制按钮组件
 }
+
 function CheckBox(props: any) {
   const {
     type = 'default',
@@ -22,53 +22,51 @@ function CheckBox(props: any) {
     name,
     label,
     indeterminate
-  }: Iprops = props
+  }: CheckBoxProps = props
   const [checkBoxChecked, setCheckBoxChecked] = useState(checked || false)
   const [isIndeterminate, setIsIndeterminate] = useState(false)
-  useEffect(() => {
-    //  监听 初始化判断是否为 多选组选项行为
-    {
-      setCheckBoxChecked(false)
-    }
-  }, [props.checkGroupValue]) // eslint-disable-line
-  useEffect(() => {
-    setCheckBoxChecked(checked)
-  }, [props.checked]) // eslint-disable-line
 
   useEffect(() => {
-    console.log(checkBoxChecked, 3333)
-  }, [checkBoxChecked]) // eslint-disable-line
+    setCheckBoxChecked(checked)
+  }, [checked])
+
   useEffect(() => {
-    setIsIndeterminate(indeterminate ? indeterminate : false)
-  }, [indeterminate]) // eslint-disable-line
+    setIsIndeterminate(indeterminate || false)
+  }, [indeterminate])
+
   function handleChange(evt: any) {
     setCheckBoxChecked(evt.target.checked)
     props.onChange && props.onChange(evt)
   }
 
+  const componentClass = classnames('checkbox', {
+    'checkbox--disabled': disabled,
+    'checkbox--indeterminate': isIndeterminate,
+  });
+
   return (
     <div
       className={[
-        'tb-checkbox',
+        'violetCheckBox',
         classnames({
-          'is-disabled': disabled
+          'violetCheckBox--disabled': disabled
         })
       ].join(' ')}
     >
       <label>
         <input
-          type='checkbox'
+          type='CheckBox'
           checked={checkBoxChecked}
           name={name}
           disabled={disabled}
           onChange={handleChange}
           value={value || label}
           className={[
-            `checkbox-type_${type}`,
+            `violetCheckBox-type_${type}`,
             classnames({
-              'is-disabled': disabled,
+              'violetCheckBox--disabled': disabled,
               // 判断是否需要 全选，如果全选加标记
-              'is-indeterminate': isIndeterminate
+              'violetCheckBox--indeterminate': isIndeterminate
             })
           ].join(' ')}
         />
@@ -81,7 +79,6 @@ function CheckBox(props: any) {
 CheckBox.defaultProps = {
   disabled: false,
   type: 'default',
-}
+};
 
-export default CheckBox
-
+export default CheckBox;
