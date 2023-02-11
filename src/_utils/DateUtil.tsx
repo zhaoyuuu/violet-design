2f4df9c5dffa02c6aeae93dd51a3392b1ce3c5de
李玮 <505805473@qq.com>
2023-02-11 22:31:05 +0800

    feat:add calendar

---
+++ src/_utils/DateUtil.tsx
@@ -0,0 +1,209 @@
+import { IDatePicker } from './@types'
+import dayjs from 'dayjs'
+import localeData from 'dayjs/plugin/localeData'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import weekday from 'dayjs/plugin/weekday'
+
+/*常量 */
+export const DatePickerDefaults = {
+  monthFormat: 'YYYY-MM',
+  dateFormat: 'YYYY-MM-DD',
+  dateTimeFormat: 'YYYY-MM-DD HH:mm A',
+  timeFormat: 'HH:mm A',
+  locale: 'en',
+}
+
+/*StringUtil */
+export const lpad = (val: string, length: number, char: '0') =>
+  val.length < length ? char.repeat(length - val.length) + val : val
+
+/*ArrayUtil */
+export const chunk = (arr: any[], n: number) => {
+  const result = []
+  let i = 0
+  while (i < arr.length / n) {
+    result.push(arr.slice(i * n, i * n + n))
+    i += 1
+  }
+
+  return result
+}
+
+/*TypeUtil */
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type Merge<M, N> = Omit<M, Extract<keyof M, keyof N>> & N
+
+export type { Omit, Merge }
+
+/*FunctionUtil */
+export const ifExistCall = (func?: (...args: any[]) => void, ...args: any[]) =>
+  func && func(...args)
+
+/*LocaleUtil */
+dayjs.extend(localeData)
+dayjs.extend(localizedFormat)
+dayjs.extend(weekday)
+
+export const getMonthShort = (locale: IDatePicker.Locale) => {
+  dayjs.locale(locale)
+  return range(0, 12).map(v =>
+    dayjs().localeData().monthsShort(dayjs().month(v))
+  )
+}
+
+export const getWeekDays = (locale: IDatePicker.Locale) => {
+  dayjs.locale(locale)
+  return range(7).map(v =>
+    dayjs().localeData().weekdaysShort(dayjs().weekday(v))
+  )
+}
+
+export const getToday = (locale: IDatePicker.Locale) => {
+  return dayjs().locale(locale).format('LL')
+}
+
+export const range = (n1: number, n2?: number) => {
+  const result = []
+  let first = !n2 ? 0 : n1
+  let last = n2
+
+  if (!last) {
+    last = n1
+  }
+
+  while (first < last) {
+    result.push(first)
+    first += 1
+  }
+  return result
+}
+
+export const repeat = (el: any, n: number) => {
+  return range(n).map(() => el)
+}
+
+/*DOMUtil */
+const convertPx = (value: number) => `${value}px`
+/**
+ * Getting Div position as far as distance
+ * @param node
+ * @param direction
+ * @param distance
+ */
+export const getDivPosition = (
+  node: HTMLDivElement | null,
+  direction: IDatePicker.PickerDirection = IDatePicker.PickerDirection.BOTTOM,
+  height: number,
+  distance: 5
+): IDatePicker.Position => {
+  if (!node) return { left: '', top: '', bottom: '' }
+
+  let top = 0
+  let left = 0
+
+  switch (direction) {
+    case IDatePicker.PickerDirection.BOTTOM:
+      top = node.offsetTop + node.offsetHeight + distance
+      left = node.offsetLeft
+      break
+    case IDatePicker.PickerDirection.TOP:
+      top = node.offsetTop - height - distance
+      left = node.offsetLeft
+      break
+  }
+
+  return {
+    top: convertPx(top),
+    left: convertPx(left),
+  }
+}
+
+export const getDomHeight = (node: HTMLDivElement | null): number => {
+  return node ? node.clientHeight : 0
+}
+
+/*DateUtil */
+export const getDayMatrix = (year: number, month: number): string[][] => {
+  const date = dayjs().year(year).month(month)
+
+  const startOfMonth = date.startOf('month').date()
+  const endOfMonth = date.endOf('month').date()
+
+  const startDay = date.startOf('month').day()
+  const remain = (startDay + endOfMonth) % 7
+
+  return chunk(
+    [
+      ...repeat(' ', startDay),
+      ...range(startOfMonth, endOfMonth + 1).map(v => `${v}`),
+      ...(7 - remain === 7 ? [] : repeat(' ', 7 - remain)),
+    ],
+    7
+  )
+}
+
+export const getMonthMatrix = (locale: IDatePicker.Locale) => {
+  return chunk(getMonthShort(locale), 3)
+}
+
+export const getYearMatrix = (year: number): string[][] => {
+  return chunk(
+    range(year - 4, year + 5).map(v => `${v}`),
+    3
+  )
+}
+
+export const isYearEqual = (year1?: dayjs.Dayjs, year2?: dayjs.Dayjs) => {
+  if (!year1 || !year2) return false
+  return dayjs(year1).isSame(year2, 'year')
+}
+
+export const isDayEqual = (day1?: dayjs.Dayjs, day2?: dayjs.Dayjs) => {
+  if (!day1 || !day2) return false
+  return dayjs(day1).isSame(day2, 'date')
+}
+
+export const isYearAfter = (year1: dayjs.Dayjs, year2: dayjs.Dayjs) => {
+  return dayjs(year1).isAfter(year2, 'year')
+}
+export const isYearBefore = (year1: dayjs.Dayjs, year2: dayjs.Dayjs) => {
+  return dayjs(year1).isBefore(year2, 'year')
+}
+
+export const isMonthAfter = (month1: dayjs.Dayjs, month2: dayjs.Dayjs) => {
+  return dayjs(month1).isAfter(month2, 'month')
+}
+export const isMonthBefore = (month1: dayjs.Dayjs, month2: dayjs.Dayjs) => {
+  return dayjs(month1).isBefore(month2, 'month')
+}
+export const isDayAfter = (day1: dayjs.Dayjs, day2: dayjs.Dayjs) => {
+  return dayjs(day1).isAfter(day2, 'date')
+}
+
+export const isDayBefore = (day1: dayjs.Dayjs, day2: dayjs.Dayjs) => {
+  return dayjs(day1).isBefore(day2, 'date')
+}
+export const isDayRange = (
+  date: dayjs.Dayjs,
+  start?: dayjs.Dayjs,
+  end?: dayjs.Dayjs
+) => {
+  if (!start || !end) return false
+
+  return isDayAfter(date, start) && isDayBefore(date, end)
+}
+
+export const isYearRange = (
+  year: dayjs.Dayjs,
+  start?: dayjs.Dayjs,
+  end?: dayjs.Dayjs
+) => {
+  if (!start || !end) return false
+
+  return isYearAfter(year, start) && isYearBefore(year, end)
+}
+
+export const formatDate = (date: dayjs.Dayjs | undefined, format: string) => {
+  if (date === undefined) return ''
+  return dayjs(date).format(format)
+}
---
+++ src/components/Calendar/CalenderComponents/CalendarContainer.tsx
@@ -0,0 +1,221 @@
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import React from 'react'
+import CalendarBody from './CalendarBody'
+import CalendarHead from './CalendarHead'
+import { Props as DayViewProps } from '../DayView'
+
+import { IDatePicker } from '../../../_utils/@types'
+import { DatePickerDefaults, ifExistCall } from '../../../_utils/DateUtil'
+
+export interface CalendarContainerProps {
+  /** dayjs中支持的所有语言,使用前导入import 'dayjs/locale/zh-cn',import 'dayjs/locale/ja' */
+  locale?: IDatePicker.Locale
+  /** 日历显示或隐藏 */
+  show?: boolean
+  /** 显示或隐藏向前按钮 */
+  prevIcon?: boolean
+  /** 显示或隐藏向后按钮 */
+  nextIcon?: boolean
+  /** Event for Calendar day click */
+  onChange?: (
+    year?: dayjs.Dayjs,
+    month?: dayjs.Dayjs,
+    date?: dayjs.Dayjs
+  ) => void
+  view?: string
+}
+
+export interface PrivateProps {
+  /** CalendarContainer base prop */
+  current: dayjs.Dayjs
+  /** Default Date parameter in calendar, which is the parent component */
+  base: dayjs.Dayjs
+  /** Number of months to show at once */
+  showMonthCnt: number
+  /** Set Calendar initial Date  */
+  setBase: (base: dayjs.Dayjs) => void
+}
+
+export interface State {
+  viewMode: IDatePicker.ViewMode
+}
+
+export type InheritProps = DayViewProps & CalendarContainerProps
+export type Props = CalendarContainerProps & DayViewProps & PrivateProps
+
+export class CalendarContainer extends React.Component<Props, State> {
+  public static defaultProps = {
+    current: dayjs(),
+    show: true,
+    showMonthCnt: 1,
+    showToday: false,
+    locale: DatePickerDefaults.locale,
+  }
+
+  //获取view
+  private getViewMode(view: Props['view']) {
+    switch (view) {
+      case 'year':
+        return IDatePicker.ViewMode.YEAR
+      case 'month':
+        return IDatePicker.ViewMode.MONTH
+
+      case 'day':
+        return IDatePicker.ViewMode.DAY
+      default:
+        return IDatePicker.ViewMode.DAY
+    }
+  }
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      viewMode: this.getViewMode(props.view),
+    }
+  }
+
+  public getHeaderTitle = () => {
+    const { current } = this.props
+    const year = dayjs(current).year()
+    return {
+      [IDatePicker.ViewMode.YEAR]: `${year - 4} - ${year + 5}`,
+      [IDatePicker.ViewMode.MONTH]: `${year}`,
+      [IDatePicker.ViewMode.DAY]: dayjs(current).format('YYYY.MM'),
+    }[this.state.viewMode]
+  }
+
+  public handleTitleClick = () => {
+    const { viewMode } = this.state
+    const { showMonthCnt } = this.props
+    let changedMode: IDatePicker.ViewMode = viewMode
+
+    if (viewMode === IDatePicker.ViewMode.MONTH) {
+      changedMode = IDatePicker.ViewMode.YEAR
+    } else if (viewMode === IDatePicker.ViewMode.DAY) {
+      changedMode = IDatePicker.ViewMode.MONTH
+    }
+    this.setState({
+      viewMode: showMonthCnt > 1 ? IDatePicker.ViewMode.DAY : changedMode,
+    })
+  }
+
+  public handleChange = (value: string) => {
+    const { viewMode } = this.state
+    const { current, onChange, setBase, showMonthCnt, base } = this.props
+    if (!value.trim()) return
+    //表示许哟啊显示多个月份视图
+    if (showMonthCnt > 1) {
+      const date = dayjs(current).date(parseInt(value, 10)).toDate()
+      //判断一个函数是否存在并执行该函数
+      ifExistCall(onChange, undefined, undefined, date)
+      return
+    }
+
+    if (this.props.view === 'year') {
+      const year = dayjs(base).year(parseInt(value, 10))
+      ifExistCall(onChange, year, undefined, undefined)
+    } else if (this.props.view === 'month') {
+      const month = dayjs(base).month(parseInt(value, 12))
+      ifExistCall(onChange, undefined, month, undefined)
+    } else {
+      if (viewMode === IDatePicker.ViewMode.YEAR) {
+        //用于修改组件的基准日期
+        //将字符串 value 解析为整数
+        setBase(dayjs(base).year(parseInt(value, 10)))
+        this.setState({
+          viewMode: IDatePicker.ViewMode.MONTH,
+        })
+      } else if (viewMode === IDatePicker.ViewMode.MONTH) {
+        setBase(dayjs(base).month(parseInt(value, 10)))
+        this.setState({
+          viewMode: IDatePicker.ViewMode.DAY,
+        })
+      } else {
+        const date = dayjs(current).date(parseInt(value, 10))
+        ifExistCall(onChange, undefined, undefined, date)
+      }
+    }
+  }
+
+  public handleBase = (method: string) => () => {
+    const { base, setBase } = this.props
+    const { viewMode } = this.state
+    const date: any = dayjs(base)
+    if (viewMode === IDatePicker.ViewMode.YEAR) {
+      //基准日期修改为过去或未来 10 年
+      setBase(date[method](10, 'year'))
+    } else if (viewMode === IDatePicker.ViewMode.MONTH) {
+      //将基准日期修改为过去或未来 1 年
+      setBase(date[method](1, 'year'))
+    } else {
+      //将基准日期修改为过去或未来 1 个月
+      setBase(date[method](1, 'month'))
+    }
+  }
+
+  public handleToday = () => {
+    const { setBase } = this.props
+    setBase(dayjs())
+  }
+
+  public renderCalendarHead = () => {
+    const { prevIcon, nextIcon } = this.props
+    return (
+      <CalendarHead
+        onPrev={this.handleBase('subtract')}
+        onNext={this.handleBase('add')}
+        prevIcon={prevIcon}
+        nextIcon={nextIcon}
+        onTitleClick={this.handleTitleClick}
+        title={this.getHeaderTitle()}
+      />
+    )
+  }
+
+  public renderCalendarBody = () => {
+    const {
+      customDayClass,
+      customDayText,
+      disableDay,
+      selected,
+      startDay,
+      endDay,
+      onMouseOver,
+      current,
+      locale = DatePickerDefaults.locale,
+    } = this.props
+
+    return (
+      <CalendarBody
+        viewMode={this.state.viewMode}
+        current={current}
+        selected={selected}
+        startDay={startDay}
+        endDay={endDay}
+        disableDay={disableDay}
+        onClick={this.handleChange}
+        onMouseOver={onMouseOver}
+        customDayClass={customDayClass}
+        customDayText={customDayText}
+        locale={locale}
+      />
+    )
+  }
+
+  public render() {
+    const { show } = this.props
+    const calendarClass = classNames('calendar__container', {
+      'calendar--show': show,
+    })
+
+    return (
+      <div className={calendarClass}>
+        {this.renderCalendarHead()}
+        {this.renderCalendarBody()}
+      </div>
+    )
+  }
+}
+
+export default CalendarContainer
