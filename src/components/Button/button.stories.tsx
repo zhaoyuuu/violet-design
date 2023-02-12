9219896b72d04c89fb60c9a6985202a2e1575191
violetwzj <809133018@qq.com>
2023-02-12 17:17:18 +0800

    fix: modify storybook

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -20,21 +20,21 @@ Default.args = {
   children: 'Default Button',
   onClick: () => console.log(123),
 }
-
+Default.storyName = 'Defalut Button 默认按钮'
 //primary Button
 export const Primary = Template.bind({})
 Primary.args = {
   btnType: 'primary',
   children: 'Primary Button',
 }
-
+Primary.storyName = 'Primary Button 主按钮'
 //danger Button
 export const Danger = Template.bind({})
 Danger.args = {
   btnType: 'danger',
   children: 'Danger Button',
 }
-
+Danger.storyName = 'Danger Button 危险按钮'
 //link Button
 export const Link = Template.bind({})
 Link.args = {
@@ -42,20 +42,21 @@ Link.args = {
   children: 'Link Button',
   href: 'https://google.com',
 }
-
+Link.storyName = 'Link Button 链接按钮'
 //text Button
 export const Text = Template.bind({})
 Text.args = {
   btnType: 'text',
   children: 'Text Button',
 }
+Text.storyName = 'Text Button 文本按钮'
 //icon button
 export const IconButton = (args: any) => (
   <Button btnType="primary">
     <Icon icon="upload" /> Icon Button
   </Button>
 )
-IconButton.storyName = 'Icon'
+IconButton.storyName = 'Icon Button 图标按钮'
 
 //export const
 //icon button
@@ -63,7 +64,7 @@ IconButton.storyName = 'Icon'
 export const DashButton = (args: any) => (
   <Button btnType="dash">Dashed Button</Button>
 )
-DashButton.storyName = 'Dash'
+DashButton.storyName = 'Dash Button 虚线按钮'
 
 //Large Button
 export const Large = Template.bind({})
@@ -71,9 +72,11 @@ Large.args = {
   size: 'lg',
   children: 'Large Button',
 }
+Large.storyName = 'Large Button 大按钮'
 //small Button
 export const Small = Template.bind({})
 Small.args = {
   size: 'sm',
   children: 'Small Button',
 }
+Small.storyName = 'Small Button 小按钮'
