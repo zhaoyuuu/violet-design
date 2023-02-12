01a6698347f8c434cc151395b3b2a2f6b62c8901
zhubeijia <1904829708@qq.com>
2023-02-12 14:36:00 +0800

    add comments

--- src/components/CheckBox/checkbox.tsx
+++ src/components/CheckBox/checkbox.tsx
@@ -2,16 +2,34 @@ import React, { FC, useEffect, useState } from 'react'
 import classnames from 'classnames';
 
 export interface CheckBoxProps {
+  /** 样式选择 */
   type?: string, 
+  /** 是否勾选 */
   checked?: boolean, 
+  /** 是否禁用 */
   disabled?: boolean, 
+  /** 多选框的value值 */
   value?: string | number, 
+  /** name属性 */
   name?: string, 
+  /** label名称 */
   label?: string, 
+  /** 是否为统一全选控制按键 */
   indeterminate?: any, 
   onChange?: (evt: any) => void,
-  children?: React.ReactNode; // 可选的子组件
+  /** 可选的子组件 */
+  children?: React.ReactNode; 
 }
+
+/**
+ * > 多选框。
+ * 
+ * ### 何时使用
+ * 在一组可选项中进行多项选择时使用多选框；
+ * 
+ * 单独使用时可以表示两种状态之间的切换，一般用于状态标记。
+ */
+
 export const CheckBox: FC<CheckBoxProps> = (props) => {
   const {
     type = 'default',
