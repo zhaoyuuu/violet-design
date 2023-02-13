5e76d9a6798672fc118a292b5b5e688e8d5ba0ef
violetwzj <809133018@qq.com>
2023-02-13 15:33:16 +0800

    fix: solve test problems

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
-import Button, { ButtonProps} from './button';
-
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import Button, { ButtonProps } from './button'
 
 test('our first react test case', () => {
   //先取得一个wrapper对象，利用queryByText方法找element是否为
@@ -13,54 +12,56 @@ test('our first react test case', () => {
 })
 
 const defaultprops = {
-  onClick: jest.fn()
+  onClick: jest.fn(),
 }
 
 const testProps: ButtonProps = {
   btnType: 'primary',
   size: 'lg',
-  className: 'violet'
+  className: 'violet',
 }
 
 const disabledProps: ButtonProps = {
   disabled: true,
-  onClick: jest.fn()
+  onClick: jest.fn(),
 }
 
-
-
-describe('test Button component',() => {
+describe('test Button component', () => {
   it('should render the correct default button', () => {
-    const wrapper = render(<Button {... defaultprops}>Nice</Button>)
+    const wrapper = render(<Button {...defaultprops}>Nice</Button>)
     const element = wrapper.getByText('Nice') as HTMLButtonElement
     //直接获取网页上的dom
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('BUTTON')
     expect(element.disabled).toBeFalsy()
-    expect(element).toHaveClass('violetButton violetButton-default ')
+    expect(element).toHaveClass('violetButton violetButton--default ')
     fireEvent.click(element)
     expect(defaultprops.onClick).toHaveBeenCalled()
   })
 
   it('should render the correct component based on different props', () => {
-    const wrapper = render(<Button {... testProps}>Nice</Button>)
+    const wrapper = render(<Button {...testProps}>Nice</Button>)
     const element = wrapper.getByText('Nice')
     //直接获取网页上的dom
     expect(element).toBeInTheDocument()
-    expect(element).toHaveClass('violetButton-primary violetButton-lg violet')
+    expect(element).toHaveClass('violetButton--primary violetButton--lg violet')
   })
 
   it('should render a link when btnType equals link and href is provided', () => {
-    const wrapper = render(<Button btnType='link' href="http://dummyurl">Link</Button>)
+    const wrapper = render(
+      <Button btnType="link" href="http://dummyurl">
+        Link
+      </Button>
+    )
     const element = wrapper.getByText('Link')
     //直接获取网页上的dom
     expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('A')
-    expect(element).toHaveClass('violetButton violetButton-link')
+    expect(element).toHaveClass('violetButton violetButton--link')
   })
 
   it('should render disabled button', () => {
-    const wrapper = render(<Button {... disabledProps}>Nice</Button>)
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
     const element = wrapper.getByText('Nice') as HTMLButtonElement
     //直接获取网页上的dom
     expect(element).toBeInTheDocument()
@@ -68,7 +69,4 @@ describe('test Button component',() => {
     fireEvent.click(element)
     expect(disabledProps.onClick).not.toHaveBeenCalled()
   })
-
 })
-
-
